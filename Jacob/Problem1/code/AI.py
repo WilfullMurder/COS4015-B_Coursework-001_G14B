a5b4eafd46ea05c1b8d2b3d9e136e8b9bfb09c35
WilfullMurder <81480788+WilfullMurder@users.noreply.github.com>
2022-11-17 20:08:20 +0000

    Add files via upload

--- Jacob/Problem1/code/AI.py
+++ Jacob/Problem1/code/AI.py
@@ -1,7 +1,50 @@
+from State import State
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn import tree
+
+"""    
+                |                |
+                |                |
+                |                |
+                |                |
+    LEFT SIDE   |    RIVER       |    RIGHT SIDE
+                |                |
+                |                |
+                |                |
+                |                |
+        0               1               2
+     
+
+ STUDENT, HAMSTER, DOG, CAT"""
+
+
 class AI:
+    startState = []
+    endState = []
+
+    numStates = 3
+    numObjects = 4
+    permutations = int(pow(numStates, numObjects))
 
     def AI(self):
-        pass
+        self.init()
 
     def init(self):
+        self.startState = State.State(0, 0, 0, 0)
+        self.endState = State.State(2, 2, 2, 2)
+
+    def run(self):
+        graph = None
+
+    def generateDecisionTree(self):
+        pass
+
+    def getSolution(self, graph=None):
+        pass
+
+    def displayGraph(self, graph=None):
+        pass
+
+    def addLeadingZeros(self, number=""):
         pass
