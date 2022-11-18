5f161a76784ece9fcc5f31e248c2a9a2a6f5c78e
WilfullMurder <81480788+WilfullMurder@users.noreply.github.com>
2022-11-18 20:07:42 +0000

    Updated main
    implemented some quick test functions for the state and data factory

--- Jacob/Problem1/code/main.py
+++ Jacob/Problem1/code/main.py
@@ -1,25 +1,44 @@
+
+import random
+
+from AI import AI
+from DataFactory import DataFactory
 from State import State
 
 
 def main():
+    testDataFactory()
+
+
+
+
+
+def testState():
     s = State
-    s.State(s, 0,0,0,0)
+    s.State(s, 0, 0, 0, 0)
     print(s.toString(s))
     print(s.isValid(s))
-    s.printVisual(s)
-    s.newState(s,"0111")
+    s.newState(s, "0111")
     print(s.toString(s))
     print(s.isValid(s))
-    s.printVisual(s)
-    s.newState(s,"0101")
+    s.newState(s, "0101")
     print(s.toString(s))
     print(s.isValid(s))
-    s.printVisual(s)
-    s.newState(s,"1012")
+    s.newState(s, "1012")
     print(s.toString(s))
     print(s.isValid(s))
     s.printVisual(s)
 
+def testDataFactory():
+    numStates = 3
+    numObjects = 4
+    permutations = int(pow(numStates, numObjects))
+    print("Number of permutations: ", permutations)
 
+    df = DataFactory
+    df.__init__(df)
+    print("Length: ", len(df.data))
+    for i in df.data:
+        print(i)
 
 main()
