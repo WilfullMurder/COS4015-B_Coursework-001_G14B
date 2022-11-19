8af9387e30d14e09e0569c5a800b4a5e587ad3f4
WilfullMurder <81480788+WilfullMurder@users.noreply.github.com>
2022-11-19 10:37:50 +0000

    Add files via upload

--- Problem/Solution/DataFactory.py
+++ Problem/Solution/DataFactory.py
@@ -1,14 +1,13 @@
 import itertools
+from CONSTANTS import LOCATION_SYNTAX as LS
 
 
 class DataFactory:
     data = []
 
     def __init__(self):
-        for i in itertools.product([0, 1, 2], [0, 1, 2], [0, 1, 2], [0, 1, 2]):
+        for i in itertools.product(LS, LS, LS, LS):
             if i not in self.data:
                 self.data.append(i)
 
         self.data.sort()
-
-
